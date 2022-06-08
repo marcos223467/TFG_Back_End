010e8aaef6381e5f8b6ab1b34996bfd789c4516f
marcos223467 <marcos223467@gmail.com>
2022-06-08 13:31:34 +0200

    Se pueden descargar cosas del back

--- src/index.js
+++ src/index.js
@@ -58,6 +58,11 @@ app.post('/upload', (req, res) => {
     })
 });
 
+app.get("/justificantes/:nombre", (req,res) =>
+{
+    res.download("./justificantes/" + req.params.nombre);
+})
+
 mongoose.connect(url, { useNewUrlParser: true }).then(() =>{
 
 	console.log('Conexión con la BDD realizada con éxito!!!')
